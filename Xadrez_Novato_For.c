1432f656f1ae0993c3e95e9256992dce7429eeca
Igor Lopes <iglopslopes@gmail.com>
2026-04-22 14:45:26 +0000

    Exercicios propostos resolução Xadrez Novato

--- Xadrez_Novato_For.c
+++ Xadrez_Novato_For.c
@@ -9,7 +9,7 @@
         for (int i = 1; i<=5; i++) {
 
             // Bispo: 5 casas na diagonal para cima e à direita
-           // Usando FOR conforme seu código original
+           // Usando FOR conforme código original
         
            printf("Cima, Direita\n");
                        
