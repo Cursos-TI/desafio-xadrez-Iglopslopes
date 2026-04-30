6b7ac4f5f0e2e05234ad3114bf22698035456053
Igor Lopes <iglopslopes@gmail.com>
2026-04-30 13:37:55 +0000

    Batalha_Naval_Aventureiro

---
+++ BatalhaNaval_Novato.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * Desafio Batalha Naval - Nível Aventureiro
+ * Objetivo: Posicionar 4 navios (Horizontal, Vertical e Diagonais) em uma matriz 10x10.
+ */
+
+int main() {
+    // Requisito: Matriz 10x10 inicializada com 0 (água)
+    int tabuleiro[10][10] = {0};
+    int tamanho = 3;
+    int valorNavio = 3;
+
+    // --- POSICIONAMENTO DOS NAVIOS ---
+    // 1. Navio Horizontal (Linha 1, Colunas 1 a 3)
+    // [1][1+i] => significa que para se fazer na horizontal, a linha [1] é fixa, variando as colunas [1+i]
+    for (int i = 0; i < tamanho; i++) {
+        tabuleiro[1][1 + i] = valorNavio;
+    }
+
+    // 2. Navio Vertical (Linhas 4 a 6, Coluna 5)
+    // [1+i][5] => significa que para se fazer na vertical, a linha [1+i] varia, e a coluna fica fixa [5]
+    for (int i = 0; i < tamanho; i++) {
+        tabuleiro[4 + i][5] = valorNavio;
+    }
+
+    // 3. Navio Diagonal 1 (Canto superior direito: baixo e direita)
+    // Fiz as coordenadas com os valores: (0,7), (1,8), (2,9)
+    for (int i = 0; i < tamanho; i++) {
+        tabuleiro[0 + i][7 + i] = valorNavio;
+    }
+
+    // 4. Navio Diagonal 2 (Canto inferior esquerdo: baixo e esquerda)
+    // Fiz as coordenadas com os valores: (7,2), (8,1), (9,0)
+    for (int i = 0; i < tamanho; i++) {
+        tabuleiro[7 + i][2 - i] = valorNavio;
+    }
+
+    // --- EXIBIÇÃO DO TABULEIRO ---
+    printf("--- TABULEIRO BATALHA NAVAL (AVENTUREIRO) ---\n\n");
+    
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            // Exibe 0 para água e 3 para navio com espaço para alinhar
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n"); // Pula linha para organizar a visualização
+    }
+
+    return 0;
+}
